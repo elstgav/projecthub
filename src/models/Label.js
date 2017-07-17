57795733569fceb9bbbf413ad8e6a20b27bb66c2
Gavin Elster <gavin.elster@me.com>
2017-07-17 10:48:36 -0700

    Use label value as ID
    Turns out the “ID” I was using was just the hex background color, which isn’t
    always unique

--- src/models/Label.js
+++ src/models/Label.js
@@ -1,10 +1,8 @@
 import { pick } from 'lodash'
 
-const ID_FROM_LABEL_CLASS = /labelstyle-(\w+)/
-
 export default class Label {
   static fromLabelElement = label => new Label({
-    id:    label.classList.value.match(ID_FROM_LABEL_CLASS)[1],
+    id:    label.textContent,
     val:   label.textContent,
     style: pick(label.style, ['backgroundColor', 'color']),
   })
