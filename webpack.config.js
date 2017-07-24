3a4a12dc89d206ca7f7eae3da43e92dd32448daf
Gavin Elster <gavin.elster@me.com>
2017-07-23 21:17:15 -0700

    Set up prod vs. dev builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 const path = require('path')
+const webpack = require('webpack')
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
 
+const isProdEnvironment = (process.env.NODE_ENV === 'production')
+const isDevEnvironment  = (process.env.NODE_ENV === 'development')
+
 module.exports = {
+  cache:   true,
+  devtool: isProdEnvironment ? 'source-map' : 'cheap-module-source-map',
+
   context: __dirname,
   entry: './src/Application.jsx',
   output: {
@@ -23,7 +30,28 @@ module.exports = {
     ],
   },
 
-  plugins: [
-    new LodashModuleReplacementPlugin(),
-  ],
+  plugins: (function configurePlugins() {
+    let plugins = [
+      new webpack.EnvironmentPlugin(['NODE_ENV']),
+      new LodashModuleReplacementPlugin(),
+    ]
+
+    if (isProdEnvironment) {
+      plugins = plugins.concat([
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.LoaderOptionsPlugin({
+          debug:    false,
+          minimize: true,
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+          beautify:  false,
+          sourceMap: false,
+          comments:  false,
+          compress:  { warnings: false },
+        }),
+      ])
+    }
+
+    return plugins
+  }()),
 }
