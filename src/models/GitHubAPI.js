52b305dabcc1f9d9dcd7b09ed07c37b3a7fd1d40
Gavin Elster <gavin.elster@me.com>
2017-11-11 16:36:17 -0800

    Move error logic into GitHubAPI model

---
+++ src/models/GitHubAPI.js
@@ -0,0 +1,96 @@
+/* eslint-disable no-alert */
+
+import axios from 'axios'
+import { oneLine } from 'common-tags'
+
+import { Storage } from 'src/models'
+
+class GitHubAPI {
+  static url = 'https://api.github.com'
+  static timeoutPeriod = 200
+
+  constructor() {
+    this.isTimedOut = false
+  }
+
+  query(endpoint) {
+    return new Promise((resolve, reject) => {
+      if (this.isTimedOut) {
+        return reject(new Error('API calls are timed out because we encountered an error'))
+      }
+
+      return Storage.get('githubToken', (settings) => {
+        const requestOptions = {}
+
+        if (settings.githubToken) {
+          requestOptions.headers = { Authorization: `token ${settings.githubToken}` }
+        }
+
+        axios
+          .get(`${GitHubAPI.url}/${endpoint}`, requestOptions)
+          .then(response => resolve(response.data))
+          .catch((error) => {
+            this.handleError(error)
+            reject(error)
+          })
+      })
+    })
+  }
+
+  getUser = user => this.query(`users/${user}`)
+
+  timeOutApiCalls() {
+    if (this.isTimedOut) return
+
+    this.isTimedOut = true
+    this.apiTimeout = window.setTimeout(() => {
+      window.clearTimeout(this.apiTimeout)
+      this.isTimedOut = false
+    }, GitHubAPI.timeoutPeriod)
+  }
+
+  handleError(error) {
+    switch (error.response.status) {
+      case 401:
+        if (error.response.data.message.includes('Bad credentials') && !this.isTimedOut) {
+          alert(oneLine`
+            It appears the API token you entered for the ProjectHub Chrome extension is
+            not valid 😕. Please go to ProjectHub’s options to learn how to set up a new
+            API key. You can access the options page by clicking the gear to the right
+            or going to your Chrome extensions page (chrome://extensions/).
+          `)
+          this.timeOutApiCalls()
+        } else {
+          console.error(error)
+          console.error(error.response)
+        }
+        break
+
+      case 403:
+        if (error.response.data.message.includes('API rate limit exceeded') && !this.isTimedOut) {
+          alert(oneLine`
+            The ProjectHub Chrome extension has reached the limit of unauthenticated API
+            calls it can make 😕. Please go to ProjectHub’s options to learn how to set
+            up an API key for unlimited access (it’s easy!). Click the gear to the right
+            or go to your Chrome extensions page (chrome://extensions/) to access
+            ProjectHub’s options.
+          `)
+          this.timeOutApiCalls()
+        } else {
+          console.error(error)
+          console.error(error.response)
+        }
+        break
+
+      case 404:
+        console.error(`Request failed with status code 404: No data found at ${error.request.responseURL}`)
+        break
+
+      default:
+        console.error(error)
+        console.error(error.response)
+    }
+  }
+}
+
+export default new GitHubAPI()
--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,4 @@
-import axios from 'axios'
-
-import { Session, Storage } from 'src/models'
+import { GitHubAPI, Session } from 'src/models'
 
 const ID_FROM_IMG_SRC = /\/u\/(\d+)\?/
 const USER_NAMES_KEY  = 'user-names'
@@ -32,27 +30,10 @@ export default class User {
   }
 
   fetchNameFromApi() {
-    Storage.get('githubToken', (settings) => {
-      const requestOptions = {}
-
-      if (settings.githubToken) {
-        requestOptions.headers = { Authorization: `token ${settings.githubToken}` }
-      }
-
-      // TODO: Create GitHub API model. Handles authentictation and standard error messages.
-      axios
-        .get(`https://api.github.com/users/${this.login}`, requestOptions)
-        .then((response) => {
-          const name = response.data.name || this.login
-          userNames[this.login] = name
-          Session.set(USER_NAMES_KEY, userNames)
-        })
-        .catch((error) => {
-          console.error(error.message)
-          // TODO: Handle bad credentials (ask to fix credentials)
-          // TODO: Handle rate limit exceeded (ask for credentials)
-          // TODO: Handle 404
-        })
+    GitHubAPI.getUser(this.login).then((data) => {
+      const name = data.name || this.login
+      userNames[this.login] = name
+      Session.set(USER_NAMES_KEY, userNames)
     })
   }
 }
