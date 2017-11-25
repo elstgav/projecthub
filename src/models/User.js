556dab6067e884ee2740ddd5e63dc9d9576649e1
Gavin Elster <gavin.elster@me.com>
2017-11-25 12:09:49 -0800

    Move constants into static properties

--- src/models/User.js
+++ src/models/User.js
@@ -1,14 +1,14 @@
 import { GitHubAPI, Session } from 'src/models'
 
-const ID_FROM_IMG_SRC = /\/u\/(\d+)\?/
-const USER_NAMES_KEY  = 'user-names'
-
-const userNames = Session.get(USER_NAMES_KEY) || {}
-const fetchedNames = {}
-
 export default class User {
+  static ID_FROM_IMG_SRC = /\/u\/(\d+)\?/
+  static USER_NAMES_KEY = 'user-names'
+
+  static names = Session.get(User.USER_NAMES_KEY) || {}
+  static fetchedNames = {}
+
   static fromAvatarElement = avatar => new User({
-    id:     avatar.src.match(ID_FROM_IMG_SRC)[1],
+    id:     avatar.src.match(User.ID_FROM_IMG_SRC)[1],
     login:  avatar.alt.substring(1),
     avatar: avatar.src,
   })
@@ -20,14 +20,14 @@ export default class User {
   }
 
   get name() {
-    let name = userNames[this.login]
+    let name = User.names[this.login]
 
     if (!name) {
       name = this.login
 
-      if (!fetchedNames[name]) {
+      if (!User.fetchedNames[name]) {
         this.fetchNameFromApi()
-        fetchedNames[name] = true
+        User.fetchedNames[name] = true
       }
     }
 
@@ -37,7 +37,7 @@ export default class User {
   async fetchNameFromApi() {
     const user = await GitHubAPI.getUser(this.login)
     const name = user.name || this.login
-    userNames[this.login] = name
-    Session.set(USER_NAMES_KEY, userNames)
+    User.names[this.login] = name
+    Session.set(User.USER_NAMES_KEY, User.names)
   }
 }
