2a26a161cd6994683a938397a096e8e514b50c15
Gavin Elster <gavin.elster@me.com>
2017-11-26 10:11:19 -0800

    Rewrite as PureComponents

--- src/components/SelectButton/OptionVal.jsx
+++ src/components/SelectButton/OptionVal.jsx
@@ -3,49 +3,66 @@ import PropTypes from 'prop-types'
 import { pick }  from 'lodash'
 
 import { Label, User } from 'src/models'
+import { Aux } from 'src/utils'
 
-import Avatar from 'src/components/Avatar'
+import Avatar from 'components/Avatar'
+
+/* eslint-disable react/no-multi-comp */
 
 
-const OptionVal = ({ option }) => {
-  switch (option.constructor) {
-    case Label:
-      return (
-        <div className="label-select-menu select-menu-item-text css-truncate">
-          <div className="color-label-wrapper css-truncate-target">
-            <span className="color" style={pick(option.style, ['backgroundColor'])}>&nbsp;</span>
-            &nbsp;
-            <span className="name">{option.val}</span>
-          </div>
-        </div>
-      )
+export default class OptionVal extends React.PureComponent {
+  static propTypes = {
+    option: PropTypes.oneOfType([
+      PropTypes.shape({ id: PropTypes.string, val: PropTypes.string }),
+      PropTypes.instanceOf(Label),
+      PropTypes.instanceOf(User),
+    ]).isRequired,
+  }
 
-    case User:
-      return (
-        <div>
-          <div className="select-menu-item-gravatar"><Avatar user={option} size={30} /></div>
-          <div className="select-menu-item-text">
-            {option.name}
-            {option.login !== option.name && (
-              <span className="description-inline"> {option.login}</span>
-            )}
-          </div>
-        </div>
-      )
+  render() {
+    switch (this.props.option.constructor) {
+      case Label:
+        return <LabelOption label={this.props.option} />
 
-    default:
-      return (
-        <div className="select-menu-item-text">{option.val}</div>
-      )
+      case User:
+        return <UserOption user={this.props.option} />
+
+      default:
+        return <div className="select-menu-item-text">{this.props.option.val}</div>
+    }
   }
 }
 
-OptionVal.propTypes = {
-  option: PropTypes.oneOfType([
-    PropTypes.shape({ id: PropTypes.string, val: PropTypes.string }),
-    PropTypes.instanceOf(Label),
-    PropTypes.instanceOf(User),
-  ]).isRequired,
+class LabelOption extends React.PureComponent {
+  static propTypes = { label: PropTypes.instanceOf(Label).isRequired }
+
+  render = () => (
+    <div className="label-select-menu select-menu-item-text css-truncate">
+      <div className="color-label-wrapper css-truncate-target">
+        <span className="color" style={pick(this.props.label.style, ['backgroundColor'])}>
+          &nbsp;
+        </span>
+        &nbsp;
+        <span className="name">{this.props.label.val}</span>
+      </div>
+    </div>
+  )
 }
 
-export default OptionVal
+class UserOption extends React.PureComponent {
+  static propTypes = { user: PropTypes.instanceOf(User).isRequired }
+
+  render = () => (
+    <Aux>
+      <div className="select-menu-item-gravatar">
+        <Avatar user={this.props.user} size={30} />
+      </div>
+      <div className="select-menu-item-text">
+        {this.props.user.name}
+        {this.props.user.login !== this.props.user.name && (
+          <span className="description-inline"> {this.props.user.login}</span>
+        )}
+      </div>
+    </Aux>
+  )
+}
