e86bf250e9248316d9db59085ff4125fd4c8ef9a
Gavin Elster <gavin.elster@me.com>
2017-12-31 11:36:31 -0800

    Upgrade jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   setupFiles: [
     'mock-local-storage',
-    'raf/polyfill', // Request animation frame polyfill; required for React
 
     './test/support/chrome',
     './test/support/enzyme',
